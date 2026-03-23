ea26e16c9112fb6d72379ec5dcbf74191c796794
cxk4rm9yk5-netizen <happyking671@icloud.com>
2026-03-23 22:27:55 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { createWeb3Modal } from '@web3modal/wagmi/react';
-import { defaultWagmiConfig } from '@web3modal/wagmi/react/config';
+import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react';
 import { WagmiProvider } from 'wagmi';
-import { bsc, mainnet, polygon } from 'wagmi/chains';
+import { mainnet, bsc, polygon } from 'wagmi/chains';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
-const projectId = '4c424a5697793d2581c205364188b49e';
+const projectId = '4c424a5697793d2581c205364188b49e'; 
 
 const metadata = {
   name: 'Nexus Lab',
-  description: 'Technical Recovery Portal',
+  description: 'Technical Gateway',
   url: 'https://nexus-lab-lxr9.vercel.app',
   icons: ['https://avatars.githubusercontent.com/u/37784886']
 };
@@ -20,7 +19,12 @@ const metadata = {
 const chains = [mainnet, bsc, polygon];
 const config = defaultWagmiConfig({ chains, projectId, metadata });
 
-createWeb3Modal({ wagmiConfig: config, projectId, chains, themeMode: 'dark' });
+createWeb3Modal({ 
+  wagmiConfig: config, 
+  projectId, 
+  enableAnalytics: false,
+  themeMode: 'dark' 
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
