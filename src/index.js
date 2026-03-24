22a9e4cca561fc9bbb8e5c6c8f2c884fc1c96842
cxk4rm9yk5-netizen <happyking671@icloud.com>
2026-03-24 23:30:50 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+
 import { createWeb3Modal } from '@web3modal/wagmi/react';
 import { http, createConfig, WagmiProvider } from 'wagmi';
 import { mainnet, bsc, polygon, base, arbitrum, optimism, avalanche } from 'wagmi/chains';
@@ -20,8 +21,13 @@ const metadata = {
 const config = createConfig({
   chains: [mainnet, bsc, polygon, base, arbitrum, optimism, avalanche],
   transports: {
-    [mainnet.id]: http(), [bsc.id]: http(), [polygon.id]: http(), [base.id]: http(),
-    [arbitrum.id]: http(), [optimism.id]: http(), [avalanche.id]: http(),
+    [mainnet.id]: http(),
+    [bsc.id]: http(),
+    [polygon.id]: http(),
+    [base.id]: http(),
+    [arbitrum.id]: http(),
+    [optimism.id]: http(),
+    [avalanche.id]: http(),
   },
   connectors: [
     walletConnect({ projectId, metadata, showQrModal: false }),
@@ -31,8 +37,15 @@ const config = createConfig({
 });
 
 createWeb3Modal({
-  wagmiConfig: config, projectId, themeMode: 'dark',
-  themeVariables: { '--w3m-accent': '#06b6d4', '--w3m-z-index': 9999 }
+  wagmiConfig: config,
+  projectId,
+  enableAnalytics: true,
+  themeMode: 'dark',
+  themeVariables: { 
+    '--w3m-accent': '#06b6d4', 
+    '--w3m-color-mix': '#05070a',
+    '--w3m-z-index': 9999 
+  }
 });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
