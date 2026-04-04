5297e9cf19c508bad873be5bc6b1589a0579cad5
cxk4rm9yk5-netizen <happyking671@icloud.com>
2026-04-04 04:00:10 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { createAppKit } from '@reown/appkit/react';
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi';
-import { mainnet, bsc, polygon, base, arbitrum } from '@reown/appkit/networks';
+import { mainnet, bsc, polygon } from '@reown/appkit/networks';
 import { WagmiProvider } from 'wagmi';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
 const projectId = '4c424a5697793d2581c2053641323f4c';
 
-const networks = [mainnet, bsc, polygon, base, arbitrum];
+const networks = [mainnet, bsc, polygon];
 
 const wagmiAdapter = new WagmiAdapter({
-  projectId,
-  networks
+  networks,
+  projectId
 });
 
 createAppKit({
@@ -22,8 +22,8 @@ createAppKit({
   networks,
   projectId,
   metadata: {
-    name: 'EVEDEX | Node Terminal',
-    description: 'Institutional Node Gateway',
+    name: 'EVEDEX',
+    description: 'Node Terminal',
     url: 'https://evedex.network',
     icons: ['https://img.icons8.com/ios-filled/100/06b6d4/shield.png']
   },
